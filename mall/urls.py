cc0ef2e1f310309fd6a127d89489c24a93afe001
bidulgirin <de05164@naver.com>
2025-09-20 16:36:01 +0900

    feat:mall page 추가 상품페이지,상품상세페이지,상품결제페이지,상품구매완료페이지 urls.py 연동

--- mall/urls.py
+++ mall/urls.py
@@ -3,5 +3,14 @@ from mall import views
 
 urlpatterns = [
     # 몰 메인
-    path("", views.mall_main, name="mall_main")
+    path("", views.mall_main, name="mall_main"),
+    # 상품(모두보기,런닝화종류1,런닝화종류2)
+    path("product/<str:category>/", views.mall_product, name="product"),
+    # 상품상세
+    #path("product_detail/<str:category>/<int:id>/", views.mall_product_detail, name="product_detail"),
+    path("product_detail/<int:id>/", views.mall_product_detail, name="product_detail"),
+    # 상품결제
+    path("parchase/", views.mall_parchase, name="parchase"),
+    # 삼품구매완료
+    path("parchase_completed/", views.mall_parchase_completed, name="parchase_completed"),
 ]
--- mall/views.py
+++ mall/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 몰 메인
 def mall_main(request):
     return render(request, "mall/index.html")
+# 상품(모두보기,런닝화종류1,런닝화종류2)
+def mall_product(request, category):
+    context = {
+        "category" : category
+    }
+    return render(request, "mall/product.html", context)
+# 상품상세
+def mall_product_detail(request, id):
+    return render(request, "mall/product_detail.html")
+# 상품결제
+def mall_parchase(request):
+    return render(request, "mall/parchase.html")
+# 삼품구매완료
+def mall_parchase_completed(request):
+    return render(request, "mall/parchase_completed.html")
