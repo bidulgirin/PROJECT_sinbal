930c3d5ffdf8c15e8e0a8e929b17c0aba3a01f30
Roosuri <gygns12533@gmail.com>
2025-09-22 17:48:17 +0900

    feat : 로그인 페이지 추가 및 커뮤니티 게시글 작성 페이지 초안

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def home(request):
     return render(request, "home/home.html")
+
+def login(request):
+    return render(request, "home/login.html")
