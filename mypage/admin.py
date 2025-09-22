60a5eeea803c6c58700a652f04e42cc48bae065b
Choi Jeonghu <ctu5004@naver.com>
2025-09-22 17:50:33 +0900

    최정후 두 번째 커밋
    로그인 관련 users 폴더 및 templates 폴더 하위에 html 파일 추가
    mypage 2차 수정본
    ignore 에 __init__ 추가

--- mypage/admin.py
+++ mypage/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from home.models import Brand, Category, Shoe, Review, Marathon, Registration, Cart, Order, OrderItem
 
 # Register your models here.
+admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(Shoe)
+admin.site.register(Review)
+admin.site.register(Marathon)
+admin.site.register(Registration)
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- mypage/models.py
+++ mypage/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from home.models import Shoe
 # Create your models here.
+class WishList(models.Model):
+    shoe = models.ForeignKey(Shoe, on_delete=models.CASCADE)
+    size = models.IntegerField()
+    quantity = models.IntegerField()
