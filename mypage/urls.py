013dfbef1fde200d07c7d6db4e8d44ff79c41daf
Lee Kyung Min <de05164@gmail.com>
2025-09-30 15:26:30 +0900

    hotfixe:신발리뷰게시판을 살리기

--- mypage/urls.py
+++ mypage/urls.py
@@ -3,7 +3,10 @@ from mypage import views
 
 urlpatterns = [
     path("<int:id>/", views.Profile, name="profile"),
+    # 주문목록
     path("<int:id>/order/", views.OrderList, name="orderlist"),
+    # 위시리스트
     path("<int:id>/mywish/", views.MyWish, name="mywish"),
+    # 위시리스트삭제
     path("<int:id>/delete/", views.WishDelete, name="wish_delete"),
 ]
