580a99e12d9abd998b720f47bd0a84f3d5cfc226
Choi Jeonghu <ctu5004@naver.com>
2025-10-01 17:53:09 +0900

    hotfix description 제외 brand, shoe 만 db 저장(정후)
    description db 제외 brand, shoe 만 db 저장으로 변경

--- crawling.py
+++ crawling.py
@@ -8,43 +8,59 @@ import django
 django.setup()
 
 from home.models import Brand, Shoe
-
+# .area-pagination .page-num1
 def shoes():
     brands = []
     names = []
     prices = []
     imges = []
+    codes = []
+    descriptions = []
 
-    url1 = requests.get("https://www.shoemarker.co.kr/ASP/Product/ProductList.asp?SCode1=02&SCode2=01&SCode3=01")
-    html = url1.content
-    soup1 = BeautifulSoup(html, 'lxml')
-    for i in range(int(soup1.select(".page-num1 > a")[0].text)):
-        url = requests.get("https://www.shoemarker.co.kr/ASP/Product/ProductList.asp?SearchType=S&SCode1=02&SCode2=01&SCode3=01&SSort=&Page="+str(1+i)+"&SPrice=0&EPrice=50")
-        html = url.content.decode('utf-8','replace') # 한글이 깨져서 넣어주었다.
-        soup = BeautifulSoup(html, 'lxml')
-        for brand in soup.select(".product-list .ns-type-bl-eb13x"):
-            brands.append(brand.text)
+    for i in range(7):
+        url1 = requests.get("https://www.shoemarker.co.kr/ASP/Product/ProductList.asp?SearchType=S&SCode1=02&SCode2=01&SCode3=0"+str(1+i)+"&SSort=&Page=01&SPrice=0&EPrice=50")
+        html1 = url1.content
+        soup1 = BeautifulSoup(html1, 'lxml')
+        for j in range(int(soup1.select_one(".page-num1 a").text)):
+            url = requests.get("https://www.shoemarker.co.kr/ASP/Product/ProductList.asp?SearchType=S&SCode1=02&SCode2=01&SCode3=0"+str(1+i)+"&SSort=&Page="+str(1+j)+"&SPrice=0&EPrice=50")
+            html = url.content.decode('utf-8','replace') # 한글이 깨져서 넣어주었다.
+            soup = BeautifulSoup(html, 'lxml')
+            for brand in soup.select(".product-list .ns-type-bl-eb13x"):
+                brands.append(brand.text)
 
-        for name in soup.select(".product-list .ns-type-bl-r a"):
-            names.append(name.text)
+            for name in soup.select(".product-list .ns-type-bl-r a"):
+                names.append(name.text)
 
-        for price in soup.select(".product-list .ns-type-bl-eb18x"):
-            if "품절" in price.text.strip():
-                prices.append(0)
-            else:
-                prices.append(price.text.strip().replace(",", ""))
+            for price in soup.select(".product-list .ns-type-bl-eb18x"):
+                if "품절" in price.text.strip():
+                    prices.append(0)
+                else:
+                    prices.append(price.text.strip().replace(",", ""))
 
-        for img in soup.select(".product-list .ly-img img"):
-            imges.append(img.get("src", ""))
+            for img in soup.select(".product-list .ly-img img"):
+                imges.append(img.get("src", ""))
 
-    return brands, names, prices, imges
+        #     for code in soup.select(".product-list div.ns-type-bl-r a"):
+        #         codes.append(code.get("href", "").split("=")[1]) from fake_useragent import UserAgent
+            
+        # for c in codes:
+        #     headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"}
+        #     url2 = requests.get("https://www.shoemarker.co.kr/ASP/Product/ProductDetail.asp?ProductCode="+str(c), headers=headers)
+        #     html2 = url2.content.decode('utf-8','replace')
+        #     soup2 = BeautifulSoup(html2, 'lxml')
 
-a = set(shoes()[0])
-for b in a:
-    c = Brand(name=b)
-    c.save()
+        #     for description in soup2.select(".product-info-box .info-list li"):
+        #         descriptions.append(description.text.strip().replace("\n", " = "), descriptions, shoes()[5], description = each_shoes[4]
 
-for d in zip(shoes()[0], shoes()[1], shoes()[2], shoes()[3]):
-    if Brand(name=d[0]):
-        f = Shoe(name=d[1], price=int(d[2]), images=d[3], brand_id = int(Brand.objects.get(name=d[0]).id))
-        f.save()
+
+    return brands, names, prices, imges, codes
+
+shoes_list = set(shoes()[0])
+for brand_list in shoes_list:
+    brand_DB = Brand(name=brand_list)
+    brand_DB.save()
+
+for each_shoes in zip(shoes()[0], shoes()[1], shoes()[2], shoes()[3]):
+    if Brand(name=each_shoes[0]):
+        shoeshoe = Shoe(name=each_shoes[1], price=int(each_shoes[2]), images=each_shoes[3], brand_id = int(Brand.objects.get(name=each_shoes[0]).id))
+        shoeshoe.save()
